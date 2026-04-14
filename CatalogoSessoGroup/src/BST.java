e76fbf0fabf1652b3a8b8a88296816dcd301c3cf
Maneco <matteoceracchi0204@icloud.com>
2026-04-14 13:29:46 +0200

    BST.java

--- CatalogoSessoGroup/src/BST.java
+++ CatalogoSessoGroup/src/BST.java
@@ -1,2 +1,62 @@
-public class BST  {
+public class BST<T extends Comparable<T>> {
+
+    Nodo<T> radice;
+
+    public void inserisci(T valore) {
+        radice = inserisciRic(radice, valore);
+    }
+
+    private Nodo<T> inserisciRic(Nodo<T> nodo, T valore) {
+        if (nodo == null) return new Nodo<>(valore);
+
+        if (valore.compareTo(nodo.valore) < 0)
+            nodo.sinistra = inserisciRic(nodo.sinistra, valore);
+        else
+            nodo.destra = inserisciRic(nodo.destra, valore);
+
+        return nodo;
+    }
+
+    public void stampa() {
+        stampaRic(radice);
+    }
+
+    private void stampaRic(Nodo<T> nodo) {
+        if (nodo != null) {
+            stampaRic(nodo.sinistra);
+            System.out.println(nodo.valore);
+            stampaRic(nodo.destra);
+        }
+    }
+    public boolean cerca(T valore) {
+        return cercaRic(radice, valore);
+    }
+
+    private boolean cercaRic(Nodo<T> nodo, T valore) {
+        if (nodo == null) return false;
+
+        if (valore.compareTo(nodo.valore) == 0)
+            return true;
+
+        if (valore.compareTo(nodo.valore) < 0)
+            return cercaRic(nodo.sinistra, valore);
+        else
+            return cercaRic(nodo.destra, valore);
+    }
+
+    public void stampaPerTipo(Class<?> tipo) {
+        stampaPerTipoRic(radice, tipo);
+    }
+
+    private void stampaPerTipoRic(Nodo<T> nodo, Class<?> tipo) {
+        if (nodo != null) {
+            stampaPerTipoRic(nodo.sinistra, tipo);
+
+            if (tipo.isInstance(nodo.valore)) {
+                System.out.println(nodo.valore);
+            }
+
+            stampaPerTipoRic(nodo.destra, tipo);
+        }
+    }
 }
